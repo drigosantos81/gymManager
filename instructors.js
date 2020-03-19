029a66ba12d73d6a4792c66b1c746f4c09bf6733
Rodrigo Emanuel <drigosantos@gmail.com>
2020-03-18 21:42:59 -0300

    Formatado arquivo JSON e salvamento.

--- instructors.js
+++ instructors.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const data = require('./data.json');
 
 // Create (POST)
 exports.post = function(req, res) {
@@ -9,17 +10,18 @@ exports.post = function(req, res) {
         // req.body.key == ""
         if (req.body[key] == "") {
             return res.send('Por favor, preencha todos os campos');
-        }
-        
+        }        
     }
 
-    fs.writeFile("data.json", JSON.stringify(req.body), function(err) {
+    data.instructors.push(req.body); // [{...}, {...}]
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
         if (err) return res.send("Write file error!");
 
         return res.redirect("/instructors");
     })
 
-    return res.send(req.body);
+    // return res.send(req.body);
 }
 // Update ()
 
